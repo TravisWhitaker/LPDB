23bc497ced2fa67ec7c06a6d3153de07fac9277a
Travis Whitaker <pi.boy.travis@gmail.com>
2013-04-05 15:53:18 -0400

    Scales right up(TM).

--- src/main.c
+++ src/main.c
@@ -7,12 +7,18 @@
 
 int main()
 {
+	FILE *serial;
+	serial = fopen("lol","wb");
 	master masterList;
-	masterList.length = 101;
-	masterList.albums = malloc(sizeof(album)*101);
+	masterList.length = 10000000;
+	masterList.albums = malloc(sizeof(album)*10000000);
 	if(masterList.albums == NULL)
 	{
 		printf("malloc failed\n");
+		return 0;
 	}
+	fwrite(&(masterList.length),sizeof(unsigned long int),1,serial);
+	fwrite(masterList.albums,sizeof(album),masterList.length,serial);
+	fclose(serial);
 	return 0;
 }
